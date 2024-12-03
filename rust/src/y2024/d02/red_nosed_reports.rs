8858a64d3c7b7b57f15c87f4d85a1715c2df9931
Megamiun <gabryelarm@gmail.com>
2024-12-03 13:27:04 +0100

    Adding solution for day 2

--- rust/src/y2024/d02/red_nosed_reports.rs
+++ rust/src/y2024/d02/red_nosed_reports.rs
@@ -23,8 +23,7 @@ fn is_safe_with_tolerance(level: &[i32], increasing: bool) -> bool {
         let lower = higher - 1;
 
         if !is_within_bounds(&level[lower..=higher], increasing) {
-            let remove_lower = (lower == 0
-                || is_within_bounds_indexed(level, lower - 1, higher, increasing))
+            let remove_lower = (lower == 0 || is_within_bounds_indexed(level, lower - 1, higher, increasing))
                 && is_safe(&level[higher..], increasing);
             let remove_higher = is_within_bounds_indexed(level, lower, higher + 1, increasing)
                 && is_safe(&level[higher + 1..], increasing);
