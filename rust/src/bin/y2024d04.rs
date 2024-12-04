88b0fdfa4afcd0b6846849d55cbc683b7ab16d5c
Megamiun <gabryelarm@gmail.com>
2024-12-04 17:06:45 +0100

    [2024][04] Solving part 1 more efficiently

--- rust/src/bin/y2024d04.rs
+++ rust/src/bin/y2024d04.rs
@@ -4,5 +4,5 @@ use aoc::run_for_files;
 use aoc::y2024::d04::ceres_search::find_all_xmas;
 
 fn main() {
-    run_for_files(2024, 4, &["sample", "input"], &find_all_xmas);
+    run_for_files(2024, 4, &["sample-small", "sample", "input"], &find_all_xmas);
 }
