51788a735dd7a4fa53a3f763e3942ec4a062ba26
Megamiun <gabryelarm@gmail.com>
2024-12-06 14:22:02 +0100

    [2024][06] Changing add to Index2D

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -1,8 +1,21 @@
-use derive_more::{Display};
+use derive_more::Display;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Display, Debug)]
 #[display("[{_0}; {_1}]")]
 pub struct Index2D(pub usize, pub usize);
+
+impl Index2D {
+    pub fn add(&self, Diff(x_diff, y_diff): Diff) -> Option<Index2D> {
+        let Index2D(x, y) = self;
+        let new_x = usize::try_from(*x as i32 + x_diff);
+        let new_y = usize::try_from(*y as i32 + y_diff);
+
+        match (new_x, new_y) {
+            (Ok(new_x_val), Ok(new_y_val)) => Some(Index2D(new_x_val, new_y_val)),
+            _ => None,
+        }
+    }
+}
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Display, Debug)]
 #[display("({_0}; {_1})")]
