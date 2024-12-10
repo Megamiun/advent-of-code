20d5442666baa743e6f5848337db375126301b33
Megamiun <gabryelarm@gmail.com>
2024-12-10 14:29:36 +0100

    [2024][09] Part 2 with LinkedList a little cleaner

--- rust/src/bin/y2024d09.rs
+++ rust/src/bin/y2024d09.rs
@@ -2,9 +2,9 @@ extern crate aoc;
 
 use aoc::run_for_files;
 use aoc::y2024::d09::disk_fragmenter::fragment;
-use aoc::y2024::d09::disk_fragmenter_linked_list::reorder;
+use aoc::y2024::d09::disk_fragmenter_linked_list::reorder_linked_list;
 
 fn main() {
     run_for_files(2024, 9, &["sample", "input"], &fragment);
-    run_for_files(2024, 9, &["sample", "input"], &reorder);
+    run_for_files(2024, 9, &["sample", "input"], &reorder_linked_list);
 }
--- rust/src/y2024/d09/disk_fragmenter_linked_list.rs
+++ rust/src/y2024/d09/disk_fragmenter_linked_list.rs
@@ -1,10 +1,8 @@
 use derive_more::Display;
-use std::collections::VecDeque;
 use std::fmt::Formatter;
-use std::iter::repeat_with;
 use std::ptr::null_mut;
 
-pub fn reorder(lines: &[String]) -> usize {
+pub fn reorder_linked_list(lines: &[String]) -> usize {
     let chuncked = &lines[0]
         .chars()
         .map(|c| c.to_digit(10).unwrap() as usize)
@@ -18,36 +16,32 @@ pub fn reorder(lines: &[String]) -> usize {
         .enumerate()
         .scan(0usize, |prev_end, (index, (file_size, blank_size))| {
             let start = *prev_end;
+            // This scan MAKES you mutate the state, instead of reusing the previous one 
             *prev_end = start + file_size + blank_size.unwrap_or(0);
             Some((start, *file_size, *prev_end, index))
         })
         .map(|(start, size, _, id)| (start, Content { file: id, size }))
         .collect::<Vec<_>>();
 
-    // Moving to SortedLinkedList somehow breaks this
-    let mut node = Node {
-        start: 0,
-        file: files[0].1,
-        next: None,
-    };
+    let mut list = SortedLinkedList::new();
 
-    files.iter().skip(1).for_each(|(start, file)| {
-        node.insert_at(*start, *file);
+    files.iter().for_each(|(start, file)| {
+        list.insert_at(*start, *file);
     });
 
     files.iter().rev().for_each(|(_, file)| {
-        node.relocate(*file);
+        list.relocate(*file);
     });
 
-    node.get_iter().map(|node| node.get_value()).sum()
+    list.get_iter().map(|node| node.get_value()).sum()
 }
 
-struct SortedLinkedList {
-    head: *mut Node,
+struct SortedLinkedList<'a> {
+    head: *mut Node<'a>,
 }
 
-impl SortedLinkedList {
-    fn new() -> SortedLinkedList {
+impl<'a> SortedLinkedList<'a> {
+    fn new() -> SortedLinkedList<'a> {
         SortedLinkedList { head: null_mut() }
     }
 
@@ -58,27 +52,19 @@ impl SortedLinkedList {
     }
 
     fn insert_at(&mut self, start: usize, file: Content) {
-        if self.head.is_null() {
-            self.head = Node::as_raw_ptr(start, file, null_mut());
+        if self.head.is_null() || start < unsafe { &mut *self.head }.start {
+            self.head = Node::get_raw_ptr(start, file, self.head);
         } else {
-            let head = &mut unsafe { *self.head };
-
-            println!("Before head: {}", head);
-            if start < head.start {
-                self.head = Node::as_raw_ptr(start, file, self.head);
-            } else {
-                head.insert_at(start, file);
-            }
-            println!("After head: {}", head)
-        };
+            unsafe { &mut *self.head }.insert_at(start, file);
+        }
     }
 
-    fn get_iter(&self) -> NodeIterator {
+    fn get_iter(&self) -> NodeIterator<'a> {
         if self.head.is_null() {
             return NodeIterator(None);
         }
 
-        NodeIterator(Some(unsafe { &*self.head }))
+        NodeIterator(Some(unsafe { *self.head }))
     }
 }
 
@@ -90,30 +76,32 @@ struct Content {
 }
 
 // TODO Check memory leaks and Implement Drop to clean up
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-struct Node {
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+struct Node<'a> {
     start: usize,
     file: Content,
-    next: Option<*mut Node>,
+    next: *mut Node<'a>,
 }
 
-impl Node {
-    fn as_raw_ptr(start: usize, file: Content, self_next: *mut Node) -> *mut Node {
+impl<'a> Node<'a> {
+    fn get_raw_ptr(start: usize, file: Content, self_next: *mut Node<'a>) -> *mut Node<'a> {
         Box::into_raw(Box::new(Node {
             start,
             file,
-            next: Some(self_next).take_if(|s| !s.is_null()),
+            next: self_next,
         }))
     }
 
     fn remove(&mut self, file: Content) {
-        if let Some(mut next_ptr) = &self.next {
-            let next = unsafe { &mut *next_ptr };
-            if next.file == file {
-                self.next = next.next;
-            } else {
-                next.remove(file);
-            }
+        if self.next.is_null() {
+            return;
+        }
+
+        let next = unsafe { &mut *self.next };
+        if next.file == file {
+            self.next = next.next;
+        } else {
+            next.remove(file);
         }
     }
 
@@ -122,16 +110,17 @@ impl Node {
             return;
         }
 
-        if let Some(mut next_ptr) = &self.next {
-            let next = unsafe { &mut *next_ptr };
-            if self.get_end() + file.size <= next.start {
-                self.next = Some(Node::as_raw_ptr(self.get_end(), file, next));
-                next.remove(file)
-            } else {
-                next.relocate(file);
-            }
+        if self.next.is_null() {
+            self.next = Node::get_raw_ptr(self.get_end(), file, null_mut());
+            return;
+        }
+
+        let next = unsafe { &mut *self.next };
+        if self.get_end() + file.size <= next.start {
+            self.next = Node::get_raw_ptr(self.get_end(), file, next);
+            next.remove(file)
         } else {
-            self.next = Some(Node::as_raw_ptr(self.get_end(), file, null_mut()));
+            next.relocate(file);
         }
     }
 
@@ -141,15 +130,10 @@ impl Node {
             return;
         }
 
-        if let Some(mut next_ptr) = &self.next {
-            let next = unsafe { &mut *next_ptr };
-            if next.start > start {
-                self.next = Some(Node::as_raw_ptr(start, file, next))
-            } else {
-                next.insert_at(start, file);
-            }
+        if self.next.is_null() || start < unsafe { &mut *self.next }.start {
+            self.next = Node::get_raw_ptr(start, file, self.next)
         } else {
-            self.next = Some(Node::as_raw_ptr(start, file, null_mut()))
+            unsafe { &mut *self.next }.insert_at(start, file);
         }
     }
 
@@ -160,45 +144,29 @@ impl Node {
     fn get_value(&self) -> usize {
         (self.start..self.get_end()).sum::<usize>() * self.file.file
     }
-
-    fn get_iter(&self) -> NodeIterator {
-        NodeIterator(Some(self))
-    }
 }
 
-struct NodeIterator<'a>(Option<&'a Node>);
+struct NodeIterator<'a>(Option<Node<'a>>);
 
 impl<'a> Iterator for NodeIterator<'a> {
-    type Item = &'a Node;
+    type Item = Node<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.map(|node| {
-            self.0 = unsafe { node.next.map(|t| &*t) };
-            node
-        })
+        let node = self.0?;
+        self.0 = Some(node.next)
+            .take_if(|n| !n.is_null())
+            .map(|t| unsafe { *t });
+        
+        Some(node)
     }
 }
 
-impl Display for Node {
+impl<'a> Display for Node<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if !self.next.is_none() {
-            write!(f, "[{}, {:?}] -> {}", self.start, self.file, unsafe { *self.next.unwrap() })
+        if !self.next.is_null() {
+            write!(f, "[{}, {:?}] -> {}", self.start, self.file, unsafe { *self.next })
         } else {
             write!(f, "[{}, {:?}]", self.start, self.file)
         }
     }
-}
-
-fn get_from_back(numbers: &mut VecDeque<usize>, total: &mut Vec<usize>, amount: usize) {
-    repeat_with(|| numbers.pop_back())
-        .take(amount)
-        .flatten()
-        .for_each(|s| total.push(s));
-}
-
-fn get_from_front(numbers: &mut VecDeque<usize>, total: &mut Vec<usize>, amount: usize) {
-    repeat_with(|| numbers.pop_front())
-        .take(amount)
-        .flatten()
-        .for_each(|s| total.push(s));
 }
