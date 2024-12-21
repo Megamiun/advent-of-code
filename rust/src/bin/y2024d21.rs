4fcc916cb749f70bebf46a6e5a98fb07b1462513
Megamiun <gabryelarm@gmail.com>
2024-12-21 20:34:54 +0100

    [2024][21] Solving part2

--- rust/src/bin/y2024d21.rs
+++ rust/src/bin/y2024d21.rs
@@ -4,5 +4,6 @@ use aoc::run_for_files;
 use aoc::y2024::d21::keypad_conundrum::get_sum_of_complexity;
 
 fn main() {
-    run_for_files(2024, 21, "Part 1", &["sample", "input"], get_sum_of_complexity);
+    run_for_files(2024, 21, "Part 1", &["sample", "input"], |lines| get_sum_of_complexity(lines, 2));
+    run_for_files(2024, 21, "Part 2", &["sample", "input"], |lines| get_sum_of_complexity(lines, 25));
 }
