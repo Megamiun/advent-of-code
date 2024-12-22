d3b8c2af37459ed90cc9dec7adba8e721fed405e
Megamiun <gabryelarm@gmail.com>
2024-12-22 01:41:55 +0100

    [2023][20] Solving part 2!

--- jvm/src/main/kotlin/br/com/gabryel/adventofcode/y2023/d20/PulsePropagation.kt
+++ jvm/src/main/kotlin/br/com/gabryel/adventofcode/y2023/d20/PulsePropagation.kt
@@ -16,60 +16,76 @@ private fun List<String>.getSignalMultiplication(): Int {
     val connectors = this
         .plus("button -> broadcaster")
         .associate(Connection::from)
-    connectors.registerOnConjuction()
+    connectors.registerOnConjunction()
 
     val button = connectors["button"]!!
-    val signalsSent = mapOf(true to AtomicInteger(), false to AtomicInteger())
+    val signalsSent = (1..1000).map {
+        connectors.clickButton(button)
+    }.reduce { acc, curr -> (acc.first + curr.first) to (acc.second + curr.second) }
 
-    repeat(1000) {
-        val signals = ArrayDeque<Triple<String, String, Boolean>>().apply {
-            addAll(button.receiveSignal("player", false))
-        }
-
-        while (signals.isNotEmpty()) {
-            val (from, to, signal) = signals.removeFirst()
-            signalsSent[signal]!!.incrementAndGet()
-
-            val toConnector = connectors[to] ?: continue
-
-            signals.addAll(toConnector.receiveSignal(from, signal))
-        }
-    }
-
-    return signalsSent[true]!!.get() * signalsSent[false]!!.get()
+    return signalsSent.first * signalsSent.second
 }
 
-private fun List<String>.getFirstCallToRx(): Int {
+private fun List<String>.getFirstCallToRx(): Long {
     val connectors = associate(Connection::from)
-    connectors.registerOnConjuction()
+    connectors.registerOnConjunction()
 
-    if (connectors.values.flatMap { it.outputs }.none { it == "rx" })
-        return -1
+    val rxPointing = connectors.entries.firstOrNull { "rx" in it.value.outputs }
+        ?: return -1
 
-    val broadcaster = connectors["broadcaster"]!!
-    generateSequence(1) { it + 1 }.forEach { buttonClick ->
-        val signals = ArrayDeque<Triple<String, String, Boolean>>().apply {
-            addAll(broadcaster.receiveSignal("button", false))
-        }
+    val clusters = connectors.clusterize(rxPointing.key)
+    val clusterCycles = connectors.findCycles(clusters)
 
-        while (signals.isNotEmpty()) {
-            val (from, to, signal) = signals.removeFirst()
-            val toConnector = connectors[to]
-
-            if (toConnector != null) {
-                signals.addAll(toConnector.receiveSignal(from, signal))
-                continue
-            }
-
-            if (to == "rx" && !signal)
-                return buttonClick
-        }
-    }
-
-    return -1
+    return clusterCycles.mapNotNull(CycleInfo::cycleLength).leastCommonMultiplier()
 }
 
-private fun Map<String, Connection>.registerOnConjuction() {
+private fun Map<String, Connection>.findCycles(clusters: Map<String, List<Connection>>): Collection<CycleInfo> {
+    val broadcaster = this["broadcaster"]!!
+
+    val clusterCycles = clusters.mapValues { (_, connections) -> CycleInfo(connections) }
+
+    generateSequence(1L) { it + 1 }.map { buttonClick ->
+        clickButton(broadcaster)
+
+        clusterCycles.values.forEach { it.updateClosedStatus(buttonClick) }
+    }.takeWhile { !clusterCycles.values.all { it.isClosed() } }.last()
+
+    return clusterCycles.values
+}
+
+private fun Map<String, Connection>.clickButton(starter: Connection): Pair<Int, Int> {
+    val signalsSent = mapOf(true to AtomicInteger(), false to AtomicInteger())
+    val signals = ArrayDeque<Triple<String, String, Boolean>>().apply {
+        addAll(starter.receiveSignal("player", false))
+    }
+
+    while (signals.isNotEmpty()) {
+        val (from, to, signal) = signals.removeFirst()
+        signalsSent[signal]!!.incrementAndGet()
+
+        val toConnector = this[to] ?: continue
+
+        signals.addAll(toConnector.receiveSignal(from, signal))
+    }
+
+    return (signalsSent[true]?.get() ?: 0) to (signalsSent[false]?.get() ?: 0)
+}
+
+private fun Map<String, Connection>.clusterize(key: String): Map<String, List<Connection>> =
+    filter { key in it.value.outputs }.map { it.key }.associateWith { clusterRoot ->
+        generateSequence(setOf(clusterRoot) to setOf<String>()) { (toVisit, visited) ->
+            val newToVisit = toVisit
+                .filter { it !in visited }
+                .flatMap { visit -> entries.filter { visit in it.value.outputs }.map { it.key } }
+                .toSet()
+
+            newToVisit to (visited + toVisit)
+        }.takeWhile { it.first.isNotEmpty() }
+            .last().second.minusElement(clusterRoot)
+            .mapNotNull { this[it] }
+    }
+
+private fun Map<String, Connection>.registerOnConjunction() {
     forEach { (name, connector) ->
         connector.outputs
             .mapNotNull { this[it] }
@@ -78,10 +94,32 @@ private fun Map<String, Connection>.registerOnConjuction() {
     }
 }
 
-private sealed class Connection {
+private fun List<Long>.leastCommonMultiplier() = reduce { acc, curr -> leastCommonMultiplier(acc, curr) }
 
+private fun leastCommonMultiplier(a: Long, b: Long) = (a * b) / greatestCommonDivisor(a, b)
+
+private fun greatestCommonDivisor(a: Long, b: Long): Long {
+    if (b == 0L) return a
+    return greatestCommonDivisor(b, a % b)
+}
+
+private class CycleInfo(private val connections: List<Connection>) {
+    var cycleLength: Long? = null
+        private set
+
+    fun updateClosedStatus(buttonClick: Long) {
+        if (cycleLength == null && connections.all { it.isResetted() }) {
+            cycleLength = buttonClick
+        }
+    }
+
+    fun isClosed() = cycleLength != null
+}
+
+private sealed class Connection {
     abstract val outputs: List<String>
     abstract fun receiveSignal(fromConnector: String, high: Boolean): List<Triple<String, String, Boolean>>
+    abstract fun isResetted(): Boolean
 
     companion object {
         private val breaker = "([&%]?)(.*) -> (.*)".toRegex()
@@ -102,18 +140,23 @@ private sealed class Connection {
 
 private data class Broadcaster(val name: String, override val outputs: List<String>): Connection() {
     override fun receiveSignal(fromConnector: String, high: Boolean) = outputs.map { Triple(name, it, high) }
+    override fun isResetted() = true
 }
 
-private class FlipFlop(val name: String, override val outputs: List<String>, private var memoryHigh: Boolean = false): Connection() {
+private data class FlipFlop(val name: String, override val outputs: List<String>): Connection() {
+    private var memoryHigh: Boolean = false
+
     override fun receiveSignal(fromConnector: String, high: Boolean): List<Triple<String, String, Boolean>> {
         if (high) return emptyList()
         memoryHigh = !memoryHigh
 
         return outputs.map { Triple(name, it, memoryHigh) }
     }
+
+    override fun isResetted() = !memoryHigh
 }
 
-private class Conjunction(val name: String, override val outputs: List<String>): Connection() {
+private data class Conjunction(val name: String, override val outputs: List<String>): Connection() {
     private val inputs = mutableMapOf<String, Boolean>()
 
     fun registerInput(name: String) {
@@ -126,4 +169,6 @@ private class Conjunction(val name: String, override val outputs: List<String>):
         return if (inputs.all { it.value }) outputs.map { Triple(name, it, false) }
         else outputs.map { Triple(name, it, true) }
     }
+
+    override fun isResetted() = inputs.values.all { !it }
 }
