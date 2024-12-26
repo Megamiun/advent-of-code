fbef98052e3a96738dafc409dbbc85794316b6fb
Megamiun <gabryelarm@gmail.com>
2024-12-26 01:29:08 +0100

    [2023][24] Throwing away bad code from last year

--- jvm/src/main/kotlin/br/com/gabryel/adventofcode/y2023/d24/NeverTellMeTheOdds.kt
+++ jvm/src/main/kotlin/br/com/gabryel/adventofcode/y2023/d24/NeverTellMeTheOdds.kt
@@ -1,124 +1,28 @@
 package br.com.gabryel.adventofcode.y2023.d24
 
+import br.com.gabryel.adventofcode.util.readLines
 import br.com.gabryel.adventofcode.y2023.d24.Vector3D.Dimension
 import br.com.gabryel.adventofcode.y2023.d24.Vector3D.Dimension.*
-import br.com.gabryel.adventofcode.util.readLines
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.math.MathContext
 import java.math.RoundingMode.HALF_EVEN
 import java.text.DecimalFormat
-import java.util.PriorityQueue
 
 fun main() {
-    listOf("sample"/*, "input"*/).forEach { file ->
+    listOf(
+        "sample" to BigDecimal(7).rangeTo(BigDecimal(27)),
+        "input" to BigDecimal(200000000000000).rangeTo(BigDecimal(400000000000000))
+    ).forEach { (file, range) ->
         val hailstones = readLines(2023, 24, file).map { it.toHailStone() }
 
-        listOf(
-            7.0..27.0,
-            200000000000000.0..400000000000000.0
-        ).forEach { range ->
-//            println("[Future XY Intersections][$range][$file] ${hailstones.getFutureIntersections(range)}")
-        }
-
-        val nonSkews = hailstones.filterNSkews(5)
-
-        val initialRock = Hailstone(Vector3D(
-            0.bigDecimal(),
-            0.bigDecimal(),
-            0.bigDecimal()),
-            Vector3D(
-                10.bigDecimal(),
-                10.bigDecimal(),
-                10.bigDecimal()))
-
-        val visited = PriorityQueue<Pair<Hailstone, BigDecimal>>(compareBy { it.second })
-        val toVisitSet = mutableSetOf<Hailstone>()
-        val toVisit = PriorityQueue<Pair<Hailstone, BigDecimal>>(compareBy { it.second }).apply {
-            add(initialRock to nonSkews.map { initialRock.findDiffAtClosestPointPerAxis(it) }.sumOf {
-                it.values.sumOf { it.first.pow(2) + it.second.pow(2) }
-            })
-        }
-
-        var iteration = 0
-        while(toVisit.isNotEmpty()) {
-            val (rock, value) = toVisit.remove()
-
-            toVisitSet.add(rock)
-            visited.add(rock to value)
-
-            println()
-            println()
-            println("----------")
-            println("${++iteration} - $rock")
-            println()
-
-            nonSkews.map {
-                println()
-                println("*****")
-                println("$it")
-                rock.findDiffAtClosestPointPerAxis(it)
-                    .onEach { (k, v) -> println("$k - ${v.first.stringified()}; ${v.second.stringified()}") }
-            }
-
-            val position = rock.position
-            val velocity = rock.velocity
-
-            val one = BigDecimal.ONE
-            val oneN = BigDecimal.ONE.negate()
-            val zero = BigDecimal.ZERO
-
-            val directions = listOf(
-                Vector3D(zero, zero, one),
-                Vector3D(zero, zero, oneN),
-                Vector3D(zero, one, one),
-                Vector3D(zero, one, oneN),
-                Vector3D(zero, oneN, one),
-                Vector3D(zero, oneN, oneN),
-                Vector3D(one, zero, zero),
-                Vector3D(one, zero, one),
-                Vector3D(one, zero, oneN),
-                Vector3D(one, one, one),
-                Vector3D(one, one, oneN),
-                Vector3D(one, oneN, one),
-                Vector3D(one, oneN, oneN),
-                Vector3D(oneN, zero, zero),
-                Vector3D(oneN, zero, one),
-                Vector3D(oneN, zero, oneN),
-                Vector3D(oneN, one, one),
-                Vector3D(oneN, one, oneN),
-                Vector3D(oneN, oneN, one),
-                Vector3D(oneN, oneN, oneN),
-            ).flatMap { listOf(it, it * 2.bigDecimal(), it * 3.bigDecimal(), it * 11.bigDecimal()) }
-
-            toVisit.addAll(
-                directions.flatMap {
-                    listOf(rock.copy(position = position + it), rock.copy(velocity = velocity + it), rock.copy(position = position + it, velocity = velocity + it))
-                }.filter { r -> Dimension.entries.all { r.velocity[it].abs() > BigDecimal("0.001") } && r !in toVisitSet }
-                .map { newRock ->
-                    val sum = nonSkews.flatMap { newRock.findDiffAtClosestPointPerAxis(it).values }
-                        .sumOf { (f, s) -> f.pow(2) + s.pow(2) }
-                    newRock to sum
-            })
-        }
+        println("[Never Tell me The Odds - Future Intersections][$file] ${hailstones.getFutureIntersections(range)}")
+//        println("[Never Tell me The Odds - Single Intersection][$file] ${hailstones.getFutureIntersections(range)}")
     }
-}
-
-private fun List<Hailstone>.filterNSkews(desired: Int): List<Hailstone> {
-    val nonSkews = mutableListOf<Hailstone>()
-
-    for (hailstone in this) {
-        if (nonSkews.none { hailstone.normalized == it.normalized }) {
-            nonSkews += hailstone
-
-            if (nonSkews.size == desired) break
-        }
-    }
-    return nonSkews
 }
 
 private fun List<Hailstone>.getFutureIntersections(
-    range: ClosedFloatingPointRange<BigDecimal>,
+    range: ClosedRange<BigDecimal>,
     d1: Dimension = X,
     d2: Dimension = Y
 ) = withIndex().sumOf { (index, hailstone1) ->
@@ -127,10 +31,8 @@ private fun List<Hailstone>.getFutureIntersections(
 
         val intersectionPoint = hailstone1.moveToIntersectionPoint(hailstone2, d1, d2)
 
-                intersectionPoint[d1] in range &&
-                intersectionPoint[d2] in range &&
-                hailstone1.isInFuture(intersectionPoint) &&
-                hailstone2.isInFuture(intersectionPoint)
+        intersectionPoint[d1] in range && intersectionPoint[d2] in range &&
+                hailstone1.isInFuture(intersectionPoint) && hailstone2.isInFuture(intersectionPoint)
     }
 }
 
@@ -151,7 +53,6 @@ private fun String.toVector3D(): Vector3D {
 }
 
 private data class Hailstone(val position: Vector3D, val velocity: Vector3D) {
-
     val normalized by lazy {
         mapOf(
             X to velocity.normalizedTo(X),
